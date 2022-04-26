19f9610508d688b1aac5695562c37172d079b443
rusdiahamid <rusdia.hamid@gmail.com>
2022-04-26 07:15:02 +0700

    factory & faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,19 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        User::create([
-            'name' => 'Rusdia Hamid',
-            'email' => 'rusdia.hamid@gmail.com',
-            'password' => bcrypt('12345')
-
-        ]);
-
-        User::create([
-            'name' => 'Sandhika Galih',
-            'email' => 'sandhikagalih@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -39,40 +27,56 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        post::create([
-            'title' => 'Laravel 5.7',
-            'slug' => 'laravel-5.7',
-            'excerpt' => 'Laravel 5.7 is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller (MVC) architectural pattern.',
-            'body' => 'Laravel 5.7 is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller (MVC) architectural pattern. It is based on Symfony, and its main components are commonly referred to as "controllers" and "views".',
-            'category_id' => 1,
-            'user_id' => 1,
-        ]);
+        Post::factory(20)->create();
 
-        post::create([
-            'title' => 'Vue.js',
-            'slug' => 'vue-js',
-            'excerpt' => 'Vue.js is a JavaScript library for building user interfaces. It is maintained by Evan You, and it is designed to be incrementally adoptable across the entire development lifecycle.',
-            'body' => 'Vue.js is a JavaScript library for building user interfaces. It is maintained by Evan You, and it is designed to be incrementally adoptable across the entire development lifecycle. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid aspernatur labore sit inventore praesentium possimus ipsam? Aperiam eum, eligendi porro quidem illo impedit quasi quae saepe laudantium rerum. Recusandae omnis doloribus placeat sequi reprehenderit veniam odio porro quos dignissimos.',
-            'category_id' => 1,
-            'user_id' => 2,
-        ]);
+        // User::create([
+        //     'name' => 'Rusdia Hamid',
+        //     'email' => 'rusdia.hamid@gmail.com',
+        //     'password' => bcrypt('12345')
 
-        Post::create([
-            'title' => 'React.js',
-            'slug' => 'react-js',
-            'excerpt' => 'React is a JavaScript library for building user interfaces. It is maintained by Facebook, and it is designed to be incrementally adoptable across the entire development lifecycle.',
-            'body' => 'React is a JavaScript library for building user interfaces. It is maintained by Facebook, and it is designed to be incrementally adoptable across the entire development lifecycle. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid aspernatur labore sit inventore praesentium possimus ipsam? Aperiam eum, eligendi porro quidem illo impedit quasi quae saepe laudantium rerum. Recusandae omnis doloribus placeat sequi reprehenderit veniam odio porro quos dignissimos.',
-            'category_id' => 2,
-            'user_id' => 1,
-        ]);
+        // ]);
 
-        Post::create([
-            'title' => 'Angular.js',
-            'slug' => 'angular-js',
-            'excerpt' => 'Angular is a JavaScript library for building user interfaces. It is maintained by Facebook, and it is designed to be incrementally adoptable across the entire development lifecycle.',
-            'body' => 'Angular is a JavaScript library for building user interfaces. It is maintained by Facebook, and it is designed to be incrementally adoptable across the entire development lifecycle. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid aspernatur labore sit inventore praesentium possimus ipsam? Aperiam eum, eligendi porro quidem illo impedit quasi quae saepe laudantium rerum. Recusandae omnis doloribus placeat sequi reprehenderit veniam odio porro quos dignissimos.',
-            'category_id' => 2,
-            'user_id' => 2,
-        ]);
+        // User::create([
+        //     'name' => 'Sandhika Galih',
+        //     'email' => 'sandhikagalih@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+
+        // post::create([
+        //     'title' => 'Laravel 5.7',
+        //     'slug' => 'laravel-5.7',
+        //     'excerpt' => 'Laravel 5.7 is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller (MVC) architectural pattern.',
+        //     'body' => 'Laravel 5.7 is a free, open-source PHP web framework, created by Taylor Otwell and intended for the development of web applications following the model–view–controller (MVC) architectural pattern. It is based on Symfony, and its main components are commonly referred to as "controllers" and "views".',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
+        // ]);
+
+        // post::create([
+        //     'title' => 'Vue.js',
+        //     'slug' => 'vue-js',
+        //     'excerpt' => 'Vue.js is a JavaScript library for building user interfaces. It is maintained by Evan You, and it is designed to be incrementally adoptable across the entire development lifecycle.',
+        //     'body' => 'Vue.js is a JavaScript library for building user interfaces. It is maintained by Evan You, and it is designed to be incrementally adoptable across the entire development lifecycle. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid aspernatur labore sit inventore praesentium possimus ipsam? Aperiam eum, eligendi porro quidem illo impedit quasi quae saepe laudantium rerum. Recusandae omnis doloribus placeat sequi reprehenderit veniam odio porro quos dignissimos.',
+        //     'category_id' => 1,
+        //     'user_id' => 2,
+        // ]);
+
+        // Post::create([
+        //     'title' => 'React.js',
+        //     'slug' => 'react-js',
+        //     'excerpt' => 'React is a JavaScript library for building user interfaces. It is maintained by Facebook, and it is designed to be incrementally adoptable across the entire development lifecycle.',
+        //     'body' => 'React is a JavaScript library for building user interfaces. It is maintained by Facebook, and it is designed to be incrementally adoptable across the entire development lifecycle. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid aspernatur labore sit inventore praesentium possimus ipsam? Aperiam eum, eligendi porro quidem illo impedit quasi quae saepe laudantium rerum. Recusandae omnis doloribus placeat sequi reprehenderit veniam odio porro quos dignissimos.',
+        //     'category_id' => 2,
+        //     'user_id' => 1,
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Angular.js',
+        //     'slug' => 'angular-js',
+        //     'excerpt' => 'Angular is a JavaScript library for building user interfaces. It is maintained by Facebook, and it is designed to be incrementally adoptable across the entire development lifecycle.',
+        //     'body' => 'Angular is a JavaScript library for building user interfaces. It is maintained by Facebook, and it is designed to be incrementally adoptable across the entire development lifecycle. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid aspernatur labore sit inventore praesentium possimus ipsam? Aperiam eum, eligendi porro quidem illo impedit quasi quae saepe laudantium rerum. Recusandae omnis doloribus placeat sequi reprehenderit veniam odio porro quos dignissimos.',
+        //     'category_id' => 2,
+        //     'user_id' => 2,
+        // ]);
     }
 }
