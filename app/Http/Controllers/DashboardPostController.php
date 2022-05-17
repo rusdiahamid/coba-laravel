fbade46e5f7819a4e1170a217b57bf03609393e6
rusdiahamid <rusdia.hamid@gmail.com>
2022-05-18 06:59:48 +0700

    Update & Delete

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -54,7 +54,6 @@ class DashboardPostController extends Controller
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
 
         Post::create($validatedData);
-
         return redirect('/dashboard/posts')->with('success', 'Post created successfully');
     }
 
@@ -79,7 +78,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -91,7 +93,23 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required',
+        ];
+
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+
+        $validatedData =  $request->validate($rules);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::where('id', $post->id)->update($validatedData);
+        return redirect('/dashboard/posts')->with('success', 'Post updated successfully');
     }
 
     /**
@@ -102,7 +120,8 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+        return redirect('/dashboard/posts')->with('success', 'Post deleted successfully');
     }
 
     public function checkSlug(Request $request)
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,9 +7,12 @@
             <h1 class="mb-3">{{ $post->title }}</h1>
 
             <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to my posts</a>
-            <a href="" class="btn btn-warning align-items-center"><span data-feather="edit"></span> Edit</a>
-            <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
-                     
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning align-items-center"><span data-feather="edit"></span> Edit</a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="btn btn-danger border-0" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span> Delete</button>
+              </form>
             <img src="https://picsum.photos/1200/400/?{{ $post->category->name }}" class="card-img-top img-fluid mt-3" alt="{{ $post->category->name }}">
          
             <article class="my-3 fs-5">
