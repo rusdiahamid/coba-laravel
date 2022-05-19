f57d8e9a715cf435dd65362d81d486f0e0228bd7
rusdiahamid <rusdia.hamid@gmail.com>
2022-05-19 12:32:07 +0700

    preview image & chnage

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,8 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use illuminate\Support\str;
+use Illuminate\Support\Facades\Storage;
+
 
 class DashboardPostController extends Controller
 {
@@ -101,6 +103,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:2048',
             'body' => 'required',
         ];
 
@@ -109,6 +112,13 @@ class DashboardPostController extends Controller
         }
 
         $validatedData =  $request->validate($rules);
+
+        if ($request->file('image')) {
+            if ($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -125,6 +135,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if ($post->image) {
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
         return redirect('/dashboard/posts')->with('success', 'Post deleted successfully');
     }
--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -6,7 +6,7 @@
 </div>
 
 <div class="col-lg-10">
-    <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+    <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5" enctype="multipart/form-data">
       @method('put')
       @csrf
         <div class="mb-3">
@@ -40,6 +40,22 @@
           </select>
         </div>
         <div class="mb-3">
+          <label for="image" class="form-label">Post Image</label>
+          <input type="hidden" name="oldImage" value="{{ $post->image }}">
+          @if ($post->image)
+            <img src="{{ asset('storage/'.$post->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">
+          @else
+            <img class="img-preview img-fluid mb-3 col-sm-5">
+          @endif
+          
+          <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+          @error('image')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+        @enderror
+        </div>
+        <div class="mb-3">
           <label for="body" class="form-label">Body</label>
           @error('body')
           <p class="text-danger">{{ $message }}</p>
@@ -65,6 +81,21 @@
   document.addEventListener('trix-file-accept', function(event) {
     event.preventDefault();
   });
+
+  function previewImage(){
+    const image = document.querySelector('#image');
+    const imgPreview = document.querySelector('.img-preview');
+
+    imgPreview.style.display = 'block';
+
+    const oFReader = new FileReader();
+    oFReader.readAsDataURL(image.files[0]);
+
+    oFReader.onload = function(oFREvent){
+      imgPreview.src = oFREvent.target.result;
+    };
+  }
+
 </script>
 
 @endsection
